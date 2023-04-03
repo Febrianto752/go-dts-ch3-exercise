a2852a330f4c4876368df3642bdc55542fafd787
Febrianto <febrianto.bekasi@gmail.com>
2023-04-03 10:17:22 +0700

    learn post request

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -8,31 +10,36 @@ import (
 )
 
 func main() {
-	res, err := http.Get("https://jsonplaceholder.typicode.com/posts/1")
+	data := map[string]interface{}{
+		"title":  "bandung",
+		"body":   "bandung adalah ibu kota jawa barat",
+		"userId": 1,
+	}
+
+	requestJson, err := json.Marshal(data)
+
+	client := &http.Client{}
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	fmt.Println(res.Body) // output : &{[] {0xc00018e000} <nil> <nil>}
-
-	body, err := ioutil.ReadAll(res.Body)
-	fmt.Println("body :", body) // output bertipe slice of byte
-
+	req, err := http.NewRequest("POST", "https://jsonplaceholder.typicode.com/posts", bytes.NewBuffer(requestJson))
+	req.Header.Set("Content-type", "application/json")
 	if err != nil {
 		log.Fatalln(err)
 	}
-	// untuk mencegah kebocoran memori atau memory leak.
+
+	res, err := client.Do(req)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
 	defer res.Body.Close()
 
-	stringBody := string(body)
-	fmt.Println("response body :", stringBody)
-	/* Contoh output :
-	   response body : {
-	     "userId": 1,
-	     "id": 1,
-	     "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-	     "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-	   }
-	*/
-	log.Println(stringBody)
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println("response body :", string(body))
 }
