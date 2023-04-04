9499f44b9d3df8e581cded82f9ed198cd296151f
Febrianto <febrianto.bekasi@gmail.com>
2023-04-04 22:12:14 +0700

    create simple testing

---
+++ service/product_service_test.go
@@ -0,0 +1,22 @@
+package service
+
+import (
+	"latihan/repository"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+var productRepository = &repository.ProductRepositoryMock{Mock: mock.Mock{}}
+var productService = ProductService{Repository: productRepository}
+
+func TestProductServiceGetOneProduct(t *testing.T) {
+	productRepository.Mock.On("FindById", "1").Return(nil)
+
+	product, err := productService.GetOneProduct("1")
+
+	assert.Nil(t, product)
+	assert.NotNil(t, err)
+	assert.Equal(t, "Product not found", err.Error(), "error response has to be 'product not found'")
+}
